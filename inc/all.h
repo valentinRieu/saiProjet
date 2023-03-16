5146d48600f58dbcbd55706a8d8f188908a2f429
Valentin Rieu <valentinless@hotmail.fr>
2023-03-16 08:12:22 +0100

    initial commit

---
+++ inc/all.h
@@ -0,0 +1,16 @@
+#ifndef ALL_H
+#define ALL_H
+
+#include "common.h"
+
+#include "utils.h"
+#include "display.h"
+#include "bresenham.h"
+#include "setup.h"
+#define X_MIN -2048
+#define X_MAX 2048
+#define Y_MIN -2048
+#define Y_MAX 2048
+
+
+#endif
